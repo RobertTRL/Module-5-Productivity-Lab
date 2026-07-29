dd18e01b771d516398de5f8278e89810dfee7ca4
Robert Toroitich <robertojnr2802@gmail.com>
2026-07-29 13:02:10 +0300

    Added comments showing routes to implement

--- server/app.py
+++ server/app.py
@@ -4,5 +4,21 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, Note, UserSchema, NoteSchema
 
+"""
+Tested endpoints:
+    -> POST /login
+    -> POST /signup
+    -> GET /me
+
+Other endpoints(not tested):
+    -> GET /notes
+    -> GET /notes/<int:id>
+    -> POST /notes
+    -> PUT /notes/<int:id>
+    -> PATCH /notes/<int:id>
+    -> DELETE /notes/<int:id>
+    
+"""
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
